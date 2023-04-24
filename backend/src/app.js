7649af779e4e201e126fe3e6c410af166250e5bc
Juhasj <antonio.junior@infoss.com.br>
2023-04-24 17:28:13 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./router');
 const app = express();
+const cors = require('cors');
+
 
 app.use(express.json());
 app.use(router);
-
+app.use(cors());
 module.exports=app
